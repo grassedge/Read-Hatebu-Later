333f13de2325678e605b84bc755eae1daa38bcc2
hatz48 <fly.me.to.the.moon1204@gmail.com>
2011-12-04 13:30:03 +0900

    #1, #8 を修正

--- chrome/content/rhl.js
+++ chrome/content/rhl.js
@@ -57,7 +57,7 @@ var ReadLater = {
         contextAddlink.addEventListener('click', function(){
             var url = gContextMenu.linkURL;
             if (!ReadLater.rhlList[url]) {
-                ReadLater.addBookmark(url);
+                ReadLater.addBookmarkByIcon(url);
                 ReadLater.rhlList[url] = {
                     url: url,
                     tag: ReadLater.rhlTag
@@ -67,52 +67,10 @@ var ReadLater = {
     },
 
     setupRhlList: function setup_rhl_list() {
-        var bookmarks = [];
-        var cache = {};
-        var LF = String.fromCharCode(10); // 改行コード LF
-        var TAB = String.fromCharCode(9); // タブコード
         var req = new XMLHttpRequest();
         req.open('GET', 'http://b.hatena.ne.jp/hatsu48/search.data', true);
-        req.onreadystatechange = function (aEvt) {
-            if (req.readyState == 4) {
-                if(req.status == 200) {
-                    var i, j, len;
-                    var data = req.responseText.split(LF);
-                    for (i = 0, len = data.length / 4 * 3; i < len; i += 3) {
-                        var tag = [];
-                        var tmp = data[i + 1].match(/\[(.*?)\]/g);
-                        if (tmp) for (j = 0; j < tmp.length; j++) {
-                            tag.push(tmp[j].slice(1,-1));
-                        }
-                        bookmarks.push({
-                            title: data[i],
-                            tag  : tag,
-                            url  : data[i + 2]
-                        });
-                    }
-                    // ブックマーク数、自分がタグ付けした日付を取得
-                    for (i = data.length / 4 * 3, j = 0, len = data.length;
-                         i < len; i++, j++) {
-                        var bookmarkNumAndDate = data[i].split(TAB);
-                        bookmarks[j].bookmarkNum = bookmarkNumAndDate[0];
-                        bookmarks[j].date        = bookmarkNumAndDate[1];
-                    }
-                    // [*あとで読む]のタグがついてるものをcacheに確保
-                    for (i = 0, len = bookmarks.length; i < len; i++) {
-                        let bookmark = bookmarks[i];
-                        for (j = 0; j < bookmark.tag.length; j++) {
-                            if (bookmark.tag[j] == ReadLater.rhlTag) {
-                                cache[bookmark.url] = bookmark;
-                            }
-                        }
-                    }
-                } else {
-                    dump("Error loading page\n");
-                }
-            }
-        };
+        req.onreadystatechange = ReadLater.setRhlList;
         req.send(null);
-        ReadLater.rhlList = cache;
     },
     
 
@@ -125,10 +83,61 @@ var ReadLater = {
           case 'tag':
             var nsISupportsString = Components.interfaces.nsISupportsString;
             ReadLater.rhlTag = this.prefs.getComplexValue('tag', nsISupportsString).data;
+            // TODO.
+            // 設定のタグが修正されるたびに呼ばれているっぽい。
+            // 無駄な更新を行ってしまっているはず。
+            ReadLater.setupRhlList();
             break;
         }
     },
 
+    // callback
+    setRhlList: function (aEvt) {
+        var tagList = {};
+        var rhlList = {};
+        var bookmarks = [];
+        var LF  = String.fromCharCode(10); // 改行コード LF
+        var TAB = String.fromCharCode(9); // タブコード
+
+        if (this.readyState != 4) { return; }
+        if (this.status == 200) {
+            var i, j, len;
+            var data = this.responseText.split(LF);
+            for (i = 0, len = data.length / 4 * 3; i < len; i += 3) {
+                var tag = [];
+                var tmp = data[i + 1].match(/\[(.*?)\]/g);
+                if (tmp) for (j = 0; j < tmp.length; j++) {
+                    tag.push(tmp[j].slice(1,-1));
+                }
+                bookmarks.push({
+                    title  : data[i],
+                    comment: data[i + 1],
+                    url    : data[i + 2],
+                    tag    : tag
+                });
+            }
+            // ブックマーク数、自分がタグ付けした日付を取得
+            for (i = data.length / 4 * 3, j = 0, len = data.length;
+                 i < len; i++, j++) {
+                var bookmarkNumAndDate = data[i].split(TAB);
+                bookmarks[j].bookmarkNum = bookmarkNumAndDate[0];
+                bookmarks[j].date        = bookmarkNumAndDate[1];
+            }
+            // [*あとで読む]のタグがついてるものを rhlList に確保
+            for (i = 0, len = bookmarks.length; i < len; i++) {
+                let bookmark = bookmarks[i];
+                tagList[bookmark.url] = bookmark;
+                if (bookmark.tag.indexOf(ReadLater.rhlTag) != -1) {
+                    rhlList[bookmark.url] = bookmark;
+                }
+            }
+        } else {
+            dump("Error loading page\n");
+        }
+        ReadLater.rhlList = rhlList;
+        ReadLater.tagList = tagList;
+    },
+    
     // callback
     toggle: function toggle (){
         dump('toggle\n');
@@ -140,14 +149,9 @@ var ReadLater = {
         if (registed == 'true') {
             icon.setAttribute('registed', 'false');
             ReadLater.deleteBookmark(url);
-            delete ReadLater.rhlList[url];
         } else {
             icon.setAttribute('registed', 'true');
-            ReadLater.addBookmark(url);
-            ReadLater.rhlList[url] = {
-                url: url,
-                tag: [ReadLater.rhlTag]
-            };
+            ReadLater.addBookmarkByIcon(url);
         }
     },
 
@@ -164,68 +168,91 @@ var ReadLater = {
         }
     },
 
-    addBookmark: function add_bookmark(url) {
-        dump('add bookmark\n');
-        var request = new XMLHttpRequest();
-        request.mozBackgroundRequest = true;
-        request.open('POST', 'http://b.hatena.ne.jp/' + RHL.User.user.name + '/add.edit.json?editer=fxaddon');
-        request.addEventListener('error', function(e){dump('error...');}, false);
-        let headers = {
-            "Content-Type": "application/x-www-form-urlencoded",
-            "Cookie":       "rk=" + RHL.User.user.rk
-        };
-        for (let [field, value] in Iterator(headers))
-            request.setRequestHeader(field, value);
+    // callback
+    addBookmarkByIcon: function add_bookmark(url) {
+        dump('add_bookmark');
+        var comment = '[' + ReadLater.rhlTag + ']';
+        if (ReadLater.tagList[url]) {
+            comment += ReadLater.tagList[url].comment;
+        }
         let query = {
-            url:url,
-            comment:'[' + ReadLater.rhlTag + ']',
-            rks: RHL.User.user.rks
+            url: url,
+            comment: comment
         };
-        request.send(net.makeQuery(query));
+        net.post('add', query);
+        ReadLater.rhlList[url] = {
+            url: url,
+            tag: [ReadLater.rhlTag],
+            comment: comment
+        };
     },
     
+    // callback
+    addBookmarkByContextMenu: function add_bookmark_by_ctx (url) {
+        
+    },
+
+    // callback
     deleteBookmark: function delete_bookmark(url) {
-        dump('delete bookmark\n');
-        var request = new XMLHttpRequest();
-        request.mozBackgroundRequest = true;
-        request.open('POST', 'http://b.hatena.ne.jp/' + RHL.User.user.name + '/api.delete_bookmark.json?editer=fxaddon');
-        request.addEventListener('error', function(e){dump('error...');}, false);
+        var comment = ReadLater.rhlList[url].comment;
+        comment = comment.replace('[' + ReadLater.rhlTag + ']', '').trim();
+        let query = {
+            url: url
+            //comment: null
+        };
+        if (comment) {
+            query.comment = comment;
+            net.post('edit', query);
+        } else {
+            net.post('delete', query);
+            delete ReadLater.tagList[url];
+        }
+        delete ReadLater.rhlList[url];
+    }
+    
+};
+
+var net = {
+    api: {
+        add   : '/add.edit.json?editer=fxaddon',
+        edit  : '/add.edit.json?editer=fxaddon',
+        delete: '/api.delete_bookmark.json?editer=fxaddon'
+    },
+
+    makeQuery: function net_makeQuery (data) {
+        let pairs = [];
+        let regexp = /%20/g;
+        let toString = Object.prototype.toString;
+        for (let k in data) {
+            if (typeof data[k] == 'undefined') continue;
+            let n = encodeURIComponent(k);
+            let v = data[k];
+            if (toString.call(v) === '[object Array]') {
+                pairs.push(v.map(function (c) {
+                    return n + '=' + encodeURIComponent(c).replace(regexp, '+');
+                }).join('&'));
+            } else {
+                pairs.push(n + '=' + encodeURIComponent(v).replace(regexp, '+'));
+            }
+        }
+        return pairs.join('&');
+    },
+
+    post: function net_post (command, query) {
+        var req = new XMLHttpRequest();
+        req.mozBackgroundRequest = true;
+        req.open('POST', 'http://b.hatena.ne.jp/' + RHL.User.user.name + net.api[command]);
+        req.addEventListener('error', function(e){dump('error...');}, false);
         let headers = {
             "Content-Type": "application/x-www-form-urlencoded",
             "Cookie":       "rk=" + RHL.User.user.rk
         };
         for (let [field, value] in Iterator(headers))
-            request.setRequestHeader(field, value);
-        let query = {
-            url:url,
-            comment:'[' + ReadLater.rhlTag + ']',
-            rks: RHL.User.user.rks
-        };
-        request.send(net.makeQuery(query));
+            req.setRequestHeader(field, value);
+        query.rks = RHL.User.user.rks;
+        req.send(net.makeQuery(query));
     }
-    
 };
 
 window.addEventListener('load',   function(e) { ReadLater.init(); }, false);
 window.addEventListener('unload', function(e) { ReadLater.shutdown(); }, false);
-
-
-var net = {};
-net.makeQuery =  function net_makeQuery (data) {
-    let pairs = [];
-    let regexp = /%20/g;
-    let toString = Object.prototype.toString;
-    for (let k in data) {
-        if (typeof data[k] == 'undefined') continue;
-        let n = encodeURIComponent(k);
-        let v = data[k];
-        if (toString.call(v) === '[object Array]') {
-            pairs.push(v.map(function (c) {
-                return n + '=' + encodeURIComponent(c).replace(regexp, '+');
-            }).join('&'));
-        } else {
-            pairs.push(n + '=' + encodeURIComponent(v).replace(regexp, '+'));
-        }
-    }
-    return pairs.join('&');
-};
